5a722f6c4f4c809eb85d7fe5dab051432c05eb3e
Giancarlo Bellido <giancarlo@coaxialhost.com>
2023-01-09 00:20:42 -0500

    feat: updated ui

--- build.js
+++ build.js
@@ -7,25 +7,33 @@ const {
 	minify,
 	concat,
 } = require("../cxl/dist/build");
+const { readFileSync } = require("fs");
 const { writeFile } = require("fs/promises");
 
-const json = require("./dataset.json");
-const result = [],
-	map = {};
-let count = 0;
-const regex = /^[a-zñáíéúó]+$/;
+// Dataset from https://en.wiktionary.org/wiki/Wiktionary:Frequency_lists#Spanish
 
-for (const key in json) {
-	if (regex.test(key) && json[key] > 10000) {
-		result[json[key]] = key;
-		map[key] = json[key];
-		count++;
-	}
+//const json = require("./dataset.json");
+const data = readFileSync("es-corpus-5000.txt", "utf8");
+const result = [];
+const regex = /^\s+\d+\.\s+([a-zñáíéúó]+)/gm;
+
+let m;
+while ((m = regex.exec(data))) {
+	result.push(m[1]);
 }
+console.log(result.length);
+writeFile("es-common.txt", result.join("\n"));
 
-console.log(`${count} words found`);
-
-writeFile("es-common.json", JSON.stringify(map), "utf8");
-writeFile("es-common.txt", result.filter((n) => n).join("\n"), "utf8");
+/*
+console.log(`${count} words fo
+writeFile("es-common.json", JSON.stringify(result), "utf8");
+writeFile(
+	"es-common.txt",
+	Object.entries(result)
+		.sort((a, b) => (a[1] > b[1] ? -1 : 1))
+		.map((r) => r[0])
+		.join("\n"),
+	"utf8"
+);*/
 
 //buildCxl();
